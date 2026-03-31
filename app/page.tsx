9ba8d206445616f9cace47ef4625c261da45c635
francoferoleto1 <39317997+francoferoleto1@users.noreply.github.com>
2026-03-31 01:28:03 -0300

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { redirect } from 'next/navigation'
+import { createServerSupabase } from '@/lib/server'
+
+export default async function Home() {
+  const supabase = await createServerSupabase()
+  const { data: { user } } = await supabase.auth.getUser()
+  if (user) redirect('/dashboard')
+  else redirect('/login')
+}
